ce766d52f7b8e1db211abd5985f50a2c71bb655b
Vishal Trivedi <vishaltrivedi92@gmail.com>
2022-06-27 11:47:59 -0500

    New Main

---
+++ TabloidMVC/Controllers/CommentController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.VisualBasic;
+using System.Security.Claims;
+using TabloidMVC.Models.ViewModels;
+using TabloidMVC.Repositories;
+
+
+namespace TabloidMVC.Controllers
+{
+    public class CommentController : Controller
+    {
+        private readonly ICommentRepository _commentRepository;
+
+        public CommentController(ICommentRepository commentRepository)
+        {
+            _commentRepository = commentRepository;
+        }
+
+    }
+}
