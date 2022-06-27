8f40680d64106acd6dc8d93ef20c91a62e96874f
e-bierschenk <bushanks@gmail.com>
2022-06-27 16:02:38 -0500

    added delete and edit to post

--- TabloidMVC/Repositories/IPostRepository.cs
+++ TabloidMVC/Repositories/IPostRepository.cs
@@ -6,9 +6,12 @@ namespace TabloidMVC.Repositories
     public interface IPostRepository
     {
         void Add(Post post);
+        void Update(Post post);
         void Delete(Post post);
         List<Post> GetAllPublishedPosts();
+        List<Post> GetAllPostsByUser(int id);
         Post GetPublishedPostById(int id);
+        Post GetPostByPostId(int id);
         Post GetUserPostById(int id, int userProfileId);
 
     }
