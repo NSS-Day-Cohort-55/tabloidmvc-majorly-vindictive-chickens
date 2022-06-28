8c23bede115e873330fe9b521de890caf09a4907
Vishal Trivedi <vishaltrivedi92@gmail.com>
2022-06-28 09:59:23 -0500

    New Main

--- TabloidMVC/Repositories/CommentRepository.cs
+++ TabloidMVC/Repositories/CommentRepository.cs
@@ -44,6 +44,26 @@ namespace TabloidMVC.Repositories
             }
         }
 
+        public void Add(Comment comment)
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                                        INSERT INTO Comment
+                                        Subject, Content
+                                        OUPUT INSERTED.ID
+                                        VALUES (@Subject, @Content)";
+                    cmd.Parameters.AddWithValue("@Subject", comment.Subject );
+                    cmd.Parameters.AddWithValue("@Content", comment.Content);
+
+                    comment.Id = (int)cmd.ExecuteScalar();
+                }
+            }
+        }
+
     }
 
 }
