579a567c4c02ec37bbaecaeda10ed1809f4ce8ef
Hulikner <hulikner@gmail.com>
2022-06-30 15:57:40 -0500

    readtime

---
+++ TabloidMVC/Models/ViewModels/PostIndexViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TabloidMVC.Models.ViewModels
+{
+    public class PostIndexViewModel
+    {
+        public List<Post> Posts { get; set; }
+        public string ReadTime(Post post)
+        {
+            int readTime = post.Content.Split().Count();
+            int readTimeMin = 0;
+            if (readTime / 265 >= 0)
+            {
+                readTimeMin = readTime / 265;
+            }
+            else
+            {
+                readTimeMin = 1;
+            }
+
+            if(readTime % 265 > 0)
+            {
+                readTimeMin++;
+            };
+
+            if(readTimeMin <= 1)
+            {
+                return $"{readTimeMin} min";
+            }
+            else
+            {
+                return $"{readTimeMin} mins";
+            }
+        }
+    }
+}
